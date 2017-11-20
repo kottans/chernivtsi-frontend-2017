ba4a6c052487e50d88e677a8a07d9e8d92e1938b
artewka <shadak1997@gmail.com>
2017-11-20 17:41:47 +0200

    add 2 homework

---
+++ homeworks/es6-basics/artewka/es6-basics.js
@@ -0,0 +1,73 @@
+// Don't modify
+function Cart () {}
+Cart.prototype.add = function (item) {
+    if (!this.goods) {
+        this.goods = [];
+    }
+    this.goods.push(item);
+};
+
+function UserCart() {
+  UserCart.prototype.__proto__ = Cart.prototype
+  
+}
+
+function Item(id, name, amount) {
+    this.id = id;
+    this.name = name;
+    this.amount = amount;
+} 
+
+UserCart.prototype.amount = function() {
+    return this.goods.reduce(function(prev,curr) {
+        return prev + curr.amount;
+    },0);
+}
+
+UserCart.prototype.clear = function () { 
+    this.goods = []; 
+    } 
+    
+UserCart.prototype.getAll = function () { 
+    return this.goods; 
+    }
+
+UserCart.prototype.updateQnt = function (id, amount) {
+    let item;
+    for (let i in this.goods) {
+        if (this.goods[i].id == id) this.goods[i].amount *= amount;
+    }
+}
+    
+
+UserCart.prototype.remove = function (id) {
+for (let i in this.goods) {
+    if (this.goods[i].id == id) this.goods.splice(i, 1);
+}
+}
+
+// Test
+const cart = new UserCart();
+cart.add(new Item(1, 'Сhair', 2000));
+cart.add(new Item(2, 'Desk', 3000));
+cart.add(new Item(3, 'Sofa', 4000));
+let amount = cart.amount();
+if (amount === 9000) {
+    console.log('Add done');
+} else {
+     console.error('Add error');
+}
+cart.updateQnt(3, 10);
+cart.remove(2);
+amount = cart.amount();
+if (amount === 42000) {
+    console.log('Modify done');
+} else {
+         console.error('Modify error');
+}
+cart.clear();
+if (cart.getAll().length === 0) {
+    console.log('Clear done');
+} else {
+    console.error('Clear error');
+}
