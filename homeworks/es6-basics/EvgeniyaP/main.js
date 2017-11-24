2777e3e82bcd3ca21c69efeb43f22e2645d2fd1b
Evgeniya <jenyapawko@gmail.com>
2017-11-24 20:44:54 +0200

    fixed code

--- homeworks/es6-basics/EvgeniyaP/main.js
+++ homeworks/es6-basics/EvgeniyaP/main.js
@@ -25,27 +25,27 @@ UserCart.prototype.amount = function() {
 
 
 UserCart.prototype.updateQnt = function(idNum, qnt) {
-    
+
     //get the quantity of specific product
     let getQnt = function(idNum, goods) {
         let count = 0;
-        for (let item in goods){
+        for (let item in goods) {
             if (goods[item].idNum === idNum) {
                 count++;
             };
         };
         return count;
     };
-    
+
     //get first object's index with the given ID
     let getObjIndex = function(idNum, goods) {
-        for (let index in goods){
+        for (let index in goods) {
             if (goods[index].idNum === idNum) {
                 return index;
             };
         };
     };
-    
+
     //get difference between the required quantity of goods and the current
     let diff = qnt - getQnt(idNum, this.goods);
     if (diff > 0) {
@@ -53,13 +53,11 @@ UserCart.prototype.updateQnt = function(idNum, qnt) {
             this.goods.push(Object.assign({}, this.goods[getObjIndex(idNum, this.goods)]))
         };
 
-    }
-
-    else {
+    } else {
         for (let i = 0; i < -diff; i++) {
             this.goods.splice(getObjIndex(idNum, this.goods), 1);
         };
-    }
+    };
 };
 
 
