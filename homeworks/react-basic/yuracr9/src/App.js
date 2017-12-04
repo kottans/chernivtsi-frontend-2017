b7a74228de4e12126012661cd52db02d3522e191
Yura Fischuk <fischuk1996@gmail.com>
2017-12-04 18:06:29 +0200

    update app.js

--- homeworks/react-basic/yuracr9/src/App.js
+++ homeworks/react-basic/yuracr9/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
+  componentWillMount() {
     fetch(this.state.myUrl)
       .then(response => {
         return response.json();
