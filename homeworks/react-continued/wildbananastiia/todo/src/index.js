cf8cec3d03cc6009d446f18b0b5684d07ea87dde
Anastasiia Nedbailo <nastia.nana@gmail.com>
2017-12-11 22:01:03 +0200

    addd local storage data saving

--- homeworks/react-continued/wildbananastiia/todo/src/index.js
+++ homeworks/react-continued/wildbananastiia/todo/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import todoApp from './reducers'
-import Root from './components/Root'
+import React from 'react';
+import { render } from 'react-dom';
+import { compose, createStore } from 'redux';
+import todoApp from './reducers';
+import Root from './components/Root';
+import persistState from 'redux-localstorage';
 
-let store = createStore(todoApp)
+const paths = 'todos';
+const config = {key: 'todos'};
+const enhancer = compose(
+  persistState(paths, config),
+)
+
+let store = createStore(todoApp, enhancer);
 
 render(
   <Root store={store} />,
