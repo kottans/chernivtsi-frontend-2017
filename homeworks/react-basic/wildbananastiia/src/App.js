baf1d35382b7664804d078fde120e3a83a93972e
Anastasiia Nedbailo <nastia.nana@gmail.com>
2017-12-17 00:37:29 +0200

    fixed review remarks

--- homeworks/react-basic/wildbananastiia/src/App.js
+++ homeworks/react-basic/wildbananastiia/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Folder from './components/Folder'
 
 class App extends Component {
   constructor() {
     super();
 
-    this.state = { treeData: [], isDisplayChild: true };
+    this.state = { treeData: [], isDisplayChild: false };
     new Renderer().getTree().then(res => this.setState({ treeData: res }));
   }
 
   render() {
-    //debugger
     return <Folder treeData={this.state.treeData.data} name='root' />
   }
 
@@ -24,36 +24,4 @@ class Renderer {
     return await fetch(url)
       .then(res => res.json())
   }
-}
-
-class Folder extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { isDisplayChild: true };
-  }
-
-  renderView(tree) {
-    if (tree) {
-      return tree.map(file => {
-        return (
-          <Folder key={file.name} treeData={file.data} name={file.name} />
-        )
-      })
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <div>
-          <div className='button-wrapper'>
-            {this.props.treeData && <button onClick={() => this.setState({ isDisplayChild: !this.state.isDisplayChild })}></button>}
-            <div>{this.props.name}</div>
-          </div>
-          {this.state.isDisplayChild && this.renderView(this.props.treeData)}
-        </div>
-      </div>
-    );
-  }
-
 }
